d738fda968fd443c7fb005d596518c49d9e5def5
Dylan Ngo <tinhnd2@smartosc.com>
2021-05-17 17:00:12 +0700

    add output

--- instance.tf
+++ instance.tf
@@ -19,9 +19,18 @@ resource "aws_instance" "example" {
       "sudo /tmp/script.sh"
     ]
   }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
+  }
+
   connection {
     user = "${var.INSTANCE_USERNAME}"
     private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
     host = self.public_ip
   }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
